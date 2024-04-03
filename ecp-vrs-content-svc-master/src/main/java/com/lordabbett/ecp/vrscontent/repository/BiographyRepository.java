1c8601d85e11729279e7ecd33fc19a9e2aee4914
Chandrakanth2806 <112073180+Chandrakanth2806@users.noreply.github.com>
2024-04-03 16:59:30 +0530

    Delete ecp-vrs-content-svc-master directory

--- ecp-vrs-content-svc-master/src/main/java/com/lordabbett/ecp/vrscontent/repository/BiographyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lordabbett.ecp.vrscontent.repository;
-
-import com.lordabbett.ecp.vrscontent.model.entity.Biography;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BiographyRepository extends CrudRepository<Biography, Integer> {
-
-}
